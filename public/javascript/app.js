93c59d6b4d37a5fe361fb162ce94283ccc481f0b
philiptaft <ptaft@gia.edu>
2020-07-29 07:53:14 -0700

    basic aspect-ratio locking implemented

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,44 @@
 $(document).ready(() => {
+  // ***begins********* ASPECT-RATIO-LOCK FUNCTIONALITY *************** //
+  let lockAspectRatio = (e) => {
+    let $checked = $(e.target);
+    const AR = $checked.val();
+    // DISABLE all height inputs
+    $('[name$="Height"]').attr("disabled", "disabled");
+  
+    $('[name$="Width"]').each((i, el) => {
+      // UPDATE all height values according to newly:checked aspect ratio
+      let currentWidth = $(el).val();
+      let height = calcHeight(currentWidth, AR);
+      //  (^) take the EXISTING val() of width, apply calcHeight fn to determine height
+      $(el).closest(".form-group").find('[name$="Height"]').val(height);
+      //  (^) reset/update each of the nearest height input values according to new AR
+  
+      // ASSIGN new event listener to each width input for keyup which will,
+      $(el).keyup((e) => {
+        let $inputTarget = $(e.target);
+        let newWidth = $inputTarget.val();
+        let height = calcHeight(newWidth, AR);
+        //  (^) take the NEW val() of width, apply calcHeight fn to determine height
+        $inputTarget.closest(".form-group").find('[name$="Height"]').val(height);
+        //  (^) set the height value on the nearest [name*=Height] input
+      });
+    });
+  };
+  
+  let unlockAspectRatio = (e) => {
+    $('[name$="Height"]').removeAttr("disabled");
+    // $('[name$="Height"]').val("");
+  };
+  
+  let calcHeight = (width, ratio = "16:9") => {
+    let aspectWidth = ratio.split(":")[0]; /* (eg) '16' */
+    let aspectHeight = ratio.split(":")[1]; /* (eg) '9' */
+    return Math.round((width / aspectWidth) * aspectHeight);
+  };
+  // ***ends*********** ASPECT-RATIO-LOCK FUNCTIONALITY *************** //
 
+  // ***begins********* UPLOAD DATA FUNCTIONALITY ********************* //
   let uploadToS3 = (e) => {
     e.preventDefault();
     // get the image filename and any other pertinent metadata
@@ -75,15 +114,25 @@ $(document).ready(() => {
 
 
   }
+  // ***ends*********** UPLOAD DATA FUNCTIONALITY ********************* //
 
-  // potentially later use of below vals to display image preview:
-  // $('.image-result-item').on('click', (e) => {
-  //   console.log('format:', e.target.getAttribute('data-format'));
-  //   console.log('width:', e.target.getAttribute('data-width'));
-  //   console.log('height:', e.target.getAttribute('data-height'));
-  //   console.log('size:', e.target.getAttribute('data-size'));
-  //   console.log('filename:', e.target.getAttribute('data-filename'));
-  // })
+  $('[name="aspect-lock"]').change(function (e) {
+    if ($(this).is(":checked")) {
+      if ($(this).val() !== "0") {
+        lockAspectRatio(e);
+      } else {
+        unlockAspectRatio(e);
+      }
+      console.log($(this).val());
+    }
+  });
+
+  // re-enable all disabled height inputs frozen
+  $('#sharp-form').submit(function(e) {
+    $(':disabled').each(function(e) {
+        $(this).removeAttr('disabled');
+    })
+  });
 
   $('#upload-form').bind('submit', uploadToS3);
 
