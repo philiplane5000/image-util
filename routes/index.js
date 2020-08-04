6fcc4616f1b47e2a2bfd3a0c1069ac3bf46ce408
Philip <philiptaft@bitbucket.org>
2020-08-04 11:47:07 -0700

    Manage-dashboard view ready

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Uploader' });
 });
 
+/* GET manage-dashboard page */
+router.get('/manage', function(req, res, next) {
+  res.render('manage-dashboard', { title: 'Manage Dashboard' });
+});
+
 module.exports = router;
