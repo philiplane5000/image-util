8ce45660f68423ffe965e608e3ba93754dfd1c6b
Philip <philiptaft@bitbucket.org>
2020-10-06 18:10:56 -0700

    Removed validation fn

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -255,19 +255,6 @@ $(document).ready(() => {
   let clipboard = (selector) => {
     new ClipboardJS(selector)
   }
-
-  let validate = (type, value) => {
-    let regex = '';
-    switch (type) {
-      case 'email':
-      regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm
-      break;
-      case 'filepath':
-      regex = /^([a-zA-Z]\:[\\])?(fakepath)?(\\)?([a-zA-Z-]+)\.(jpg)/gm;
-      break;
-    }
-    return regex.test(value);
-  }
   // ***ends*********** MISC / UTILS ********************************** //  
 
   // ASSIGN aspect ratio lock event listeners
