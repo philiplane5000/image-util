7c7463049b27b7e03963050ad79d4df664d8f668
philiptaft <ptaft@gia.edu>
2020-10-08 15:13:11 -0700

    Force deletion on all 4 image variations

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -4,11 +4,17 @@ $(document).ready(() => {
   const regEx = {
     filepath: /^([a-zA-Z]\:[\\])?(fakepath)?(\\)?([a-zA-Z-]+)\.(jpg)$/gm,
     dimension: /^\d{1,5}$/m,
-  }
+  };
   // INIT clipboardjs on selector targetting a button
   let clipboard = (selector) => {
-    new ClipboardJS(selector)
-  }
+    new ClipboardJS(selector);
+  };
+
+  // let $validate = (regex, selector) => {
+  //   const $element = $(selector);
+  //   const $val = $element.val().trim();
+  //   return regex.test($val);
+  // }
   // ***ends*********** MISC / UTILS ********************************** //
 
   // ***begins********* ASPECT-RATIO-LOCK FUNCTIONALITY *************** //
@@ -54,10 +60,98 @@ $(document).ready(() => {
   // ***ends*********** ASPECT-RATIO-LOCK FUNCTIONALITY *************** //
 
   // ***begins********* UPLOAD DATA FUNCTIONALITY ********************* //
+  let validate = (e) => {
+    let flag = true;
+    // BEGIN Validation
+    // Only in case of 'filepath' we immediately focus user to the file
+    // upload <input> and return false to block execution
+    let filepath = $("#image-file").val().trim();
+    if (!regEx.filepath.test(filepath)) {
+      console.error("Image filename does not conform to policy :>> ", filepath);
+      $("#image-file").focus();
+      return false;
+    }
+
+    // Otherwise we toggle flag and 'validation-error' class
+    let $lgWidth = $("#lgMediaWidth").val().trim();
+    if (!regEx.dimension.test($lgWidth)) {
+      $("#lgMediaWidth").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#lgMediaWidth").removeClass("validation-error");
+    }
+
+    let $lgHeight = $("#lgMediaHeight").val().trim();
+    if (!regEx.dimension.test($lgHeight)) {
+      $("#lgMediaHeight").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#lgMediaHeight").removeClass("validation-error");
+    }
+
+    let $mdWidth = $("#mdMediaWidth").val().trim();
+    if (!regEx.dimension.test($mdWidth)) {
+      $("#mdMediaWidth").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#mdMediaWidth").removeClass("validation-error");
+    }
+
+    let $mdHeight = $("#mdMediaHeight").val().trim();
+    if (!regEx.dimension.test($mdHeight)) {
+      $("#mdMediaHeight").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#mdMediaHeight").removeClass("validation-error");
+    }
+
+    let $smWidth = $("#smMediaWidth").val().trim();
+    if (!regEx.dimension.test($smWidth)) {
+      $("#smMediaWidth").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#smMediaWidth").removeClass("validation-error");
+    }
+
+    let $smHeight = $("#smMediaHeight").val().trim();
+    if (!regEx.dimension.test($smHeight)) {
+      $("#smMediaHeight").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#smMediaHeight").removeClass("validation-error");
+    }
+
+    let $xsWidth = $("#xsMediaWidth").val().trim();
+    if (!regEx.dimension.test($xsWidth)) {
+      $("#xsMediaWidth").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#xsMediaWidth").removeClass("validation-error");
+    }
+
+    let $xsHeight = $("#xsMediaHeight").val().trim();
+    if (!regEx.dimension.test($xsHeight)) {
+      $("#xsMediaHeight").addClass("validation-error");
+      flag = false;
+    } else {
+      $("#xsMediaHeight").removeClass("validation-error");
+    }
+    // END Validation
+
+    // RE-ENABLE all disabled height inputs frozen
+    if (flag === true) {
+      $(":disabled").each((i, element) => {
+        $(element).removeAttr("disabled");
+      });
+    }
+
+    return flag;
+  };
+
   let updateLabel = (e) => {
     let fileName = $(e.target).val();
-    $(e.target).next('.custom-file-label').html(fileName);
-  }
+    $(e.target).next(".custom-file-label").html(fileName);
+  };
 
   let renderMetadata = (metadata) => {
     $("#loader-gif").addClass("d-none");
@@ -65,12 +159,20 @@ $(document).ready(() => {
 
     for (data of metadata) {
       let { Bucket, ETag, Key, Location } = data;
-      let [ input, path, dims, size, mime ] = /^(\d+\/\d{2}\/\d{2}\/[a-zA-Z-]+)\.(\d+x\d+)\.(lg|md|sm|xs)\.(jpg|png)/.exec(Key);
-      let [ original , name ] = /\d+\/\d+\/\d+\/([\w\-]+)/.exec(path);
+      let [
+        input,
+        path,
+        dims,
+        size,
+        mime,
+      ] = /^(\d+\/\d{2}\/\d{2}\/[a-zA-Z-]+)\.(\d+x\d+)\.(lg|md|sm|xs)\.(jpg|png)/.exec(
+        Key
+      );
+      let [original, name] = /\d+\/\d+\/\d+\/([\w\-]+)/.exec(path);
       size = size.toUpperCase();
 
       // Remove enclosing quotation marks from ETag for HTML ease of use:
-      ETag = ETag.replace(/\"+/g, '');
+      ETag = ETag.replace(/\"+/g, "");
 
       const $s3Data = $(`
         <div class="col-sm-6 s3-result-card mb-2">
@@ -137,7 +239,6 @@ $(document).ready(() => {
       url: "/upload/s3",
       type: "POST",
       data: JSON.stringify({ Uploads: uploads }),
-      // dataType: 'json',
       contentType: "application/json",
       success: renderMetadata,
       error: function (err) {
@@ -148,7 +249,7 @@ $(document).ready(() => {
     // POST image metadata to /upload route via request body
   };
   // ***ends*********** UPLOAD DATA FUNCTIONALITY ********************* //
-  
+
   // ***begins********* MANAGE-S3 FUNCTIONALITY *********************** //
   let renderObjectsS3 = (data) => {
     $("#loader-gif").addClass("d-none");
@@ -159,12 +260,20 @@ $(document).ready(() => {
       data.Contents.forEach((obj) => {
         let { Key, LastModified, ETag, Size, StorageClass } = obj;
         let imgSrc = `https://gempeg-poc.s3.us-east-1.amazonaws.com/${Key}`;
-        let [ input, path, dims, size, mime ] = /^(\d+\/\d{2}\/\d{2}\/[a-zA-Z-]+)\.(\d+x\d+)\.(lg|md|sm|xs)\.(jpg|png)/.exec(Key);
-        let [ original , name ] = /\d+\/\d+\/\d+\/([\w\-]+)/.exec(path);
+        let [
+          input,
+          path,
+          dims,
+          size,
+          mime,
+        ] = /^(\d+\/\d{2}\/\d{2}\/[a-zA-Z-]+)\.(\d+x\d+)\.(lg|md|sm|xs)\.(jpg|png)/.exec(
+          Key
+        );
+        let [original, name] = /\d+\/\d+\/\d+\/([\w\-]+)/.exec(path);
         size = size.toUpperCase();
 
         // Remove enclosing quotation marks from ETag for HTML ease of use:
-        ETag = ETag.replace(/\"+/g, '');
+        ETag = ETag.replace(/\"+/g, "");
 
         let card = `
           <div class="col-sm-3 mb-3 mt-3">
@@ -186,8 +295,21 @@ $(document).ready(() => {
                 </li>
               </ul>
               <div class="card-body d-flex justify-content-between align-items-center">
-                <a href="${imgSrc}" class="card-link">Download</a>
-                <a href="#" class="card-link card-link-delete" data-key="${Key}" data-tag="${ETag}">Delete</a>
+                <a
+                  href="${imgSrc}"
+                  class="card-link"
+                >
+                  Download
+                </a>
+                <a 
+                  href="#"
+                  class="card-link card-link-delete"
+                  data-key="${Key}"
+                  data-tag="${ETag}"
+                  data-path="${path}"
+                >
+                  Delete
+                </a>
               </div>
             </div>
           </div>
@@ -223,37 +345,32 @@ $(document).ready(() => {
     });
   };
 
-  let deleteObjectS3 = (e) => {
-    e.preventDefault();
-    let $parentCard = $(e.target).closest(".card");
-    let $imgTop = $parentCard.find(".card-img-top");
-    let key = $(e.target).data("key");
+  let deleteObjectsS3 = (e) => {
+    const $element = $(e.target);
+    const path = $element.data("path");
+    const $group = $(`[data-path='${path}']`);
 
-    $.ajax({
-      url: `/api/object?key=${key}`,
-      type: "DELETE",
-      success: function (data) {
-        $imgTop.css({ filter: "grayscale()" });
-        $parentCard.addClass("fade-out");
-        setTimeout(() => {
-          $(e.target).closest(".card").remove();
-        }, 500);
-      },
-      error: function (err) {
-        alert("ERROR: ", err);
-      },
+    $group.each(function (index, val) {
+      let key = $(this).data("key");
+      let $parentCard = $(`[data-key="${key}"]`).closest(".card");
+      let $imgTop = $parentCard.find(".card-img-top");
+
+      $.ajax({
+        url: `/api/object?key=${key}`,
+        type: "DELETE",
+        success: (data) => {
+          $imgTop.css({ filter: "grayscale()" });
+          $parentCard.addClass("fade-out");
+          setTimeout(() => {
+            $(e.target).closest(".card").remove();
+          }, 750);
+        },
+        error: (err) => {
+          console.error("ERROR :>> ", err);
+        },
+      });
     });
   };
-
-  // let validate = async (pattern, selector) => {
-  //   const $element = $(selector);
-  //   const $val = $element.val().trim();
-  //   console.log('Performing validation on :>> ', `${pattern} / ${selector} / ${$val}`);
-
-  //   let result = await pattern.test($val);
-  //   return result
-
-  // }
 
   let resetForm = (e) => {
     e.preventDefault();
@@ -263,8 +380,11 @@ $(document).ready(() => {
       $('[name="filecheck"]').prop("checked", false);
     }
     $(".s3-result-cards").html("");
-  }
+  };
   // ***end************ MANAGE-S3 FUNCTIONALITY *********************** //
+
+  // SHOW filename as label after user triggers via 'Browse'
+  $("#image-file").on("change", updateLabel);
 
   // ASSIGN aspect ratio lock event listeners
   $('[name="aspect-lock"]').change((e) => {
@@ -277,108 +397,21 @@ $(document).ready(() => {
     }
   });
 
-  $("#sharp-form").bind("submit", (e) => {
-    // BEGIN Validation
-    let flag = true;
-
-    const filepath = $('#image-file').val().trim();
-    if (!regEx.filepath.test(filepath)) {
-      console.error('Image filename does not conform to policy :>> ', filepath);
-      $('#image-file').focus();
-      return false
-    }
-
-    const $lgWidth = $('#lgMediaWidth').val().trim();
-    if (!regEx.dimension.test($lgWidth)) {
-      $('#lgMediaWidth').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#lgMediaWidth').removeClass('validation-error');
-    }
-
-    const $lgHeight = $('#lgMediaHeight').val().trim();
-    if (!regEx.dimension.test($lgHeight)) {
-      $('#lgMediaHeight').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#lgMediaHeight').removeClass('validation-error');
-    }
-
-    const $mdWidth = $('#mdMediaWidth').val().trim();
-    if (!regEx.dimension.test($mdWidth)) {
-      $('#mdMediaWidth').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#mdMediaWidth').removeClass('validation-error');
-    }
-
-    const $mdHeight = $('#mdMediaHeight').val().trim();
-    if (!regEx.dimension.test($mdHeight)) {
-      $('#mdMediaHeight').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#mdMediaHeight').removeClass('validation-error');
-    }
-
-    const $smWidth = $('#smMediaWidth').val().trim();
-    if (!regEx.dimension.test($smWidth)) {
-      $('#smMediaWidth').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#smMediaWidth').removeClass('validation-error');
-    }
-
-    const $smHeight = $('#smMediaHeight').val().trim();
-    if (!regEx.dimension.test($smHeight)) {
-      $('#smMediaHeight').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#smMediaHeight').removeClass('validation-error');
-    }
-
-    const $xsWidth = $('#xsMediaWidth').val().trim();
-    if (!regEx.dimension.test($xsWidth)) {
-      $('#xsMediaWidth').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#xsMediaWidth').removeClass('validation-error');
-    }
-
-    const $xsHeight = $('#xsMediaHeight').val().trim();
-    if (!regEx.dimension.test($xsHeight)) {
-      $('#xsMediaHeight').addClass('validation-error');
-      flag = false;
-    } else {
-      $('#xsMediaHeight').removeClass('validation-error');
-    }
-    // END Validation
-
-    // RE-ENABLE all disabled height inputs frozen
-    if (flag === true) {
-      $(":disabled").each((i, element) => {
-        $(element).removeAttr("disabled");
-      });
-    }
-
-    return flag
-  });
+  // BIND validation function to 'sharp-form' submit
+  $("#sharp-form").bind("submit", validate);
 
   // UPLOAD resized images to S3 bucket
   $("#upload-form").bind("submit", uploadToS3);
 
-  // BIND S3 listObjects method + render results on success
+  // BIND listObjectsS3 function to manager submit btn
   $("#manage-form").bind("submit", listObjectsS3);
 
-  // DELETE object/image from S3 bucket
-  $(".s3-result-cards").on("click", ".card-link-delete", deleteObjectS3);
+  // PRESELECT filename checkbox on manage dashboard
+  $('[name="filecheck"]').prop("checked", true);
 
   // RESET manager search selections
   $('[name="reset"]').on("click", resetForm);
 
-  // PRESELECT filename checkbox on manage dashboard
-  $('[name="filecheck"]').prop('checked', true);
-
-  // SHOW filename as label after user triggers via 'Browse'
-  $('#image-file').on('change', updateLabel);
-
+  // DELETE all object/image variations on each 'Delete' link
+  $(document).on("click", ".card-link-delete", deleteObjectsS3);
 });
